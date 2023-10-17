66082ff0956b1d0c1ac1126eadb97f05970b6e7a
TJStodda <99776279+TJStodda@users.noreply.github.com>
2023-10-16 19:36:26 -0700

    almost have button functionality
    maybe window.open()?

--- sketch.js
+++ sketch.js
@@ -1,9 +1,29 @@
 //File sketches the canvas and has the draw function which is called 60 times per second by default
+//TODO: GET WORKING BUTTONS, WINDOW.OPEN()??
+var fileName;
+
+function goTo(fileName) {
+  window.open(filename);
+}
+
 
 function setup() {
   createCanvas(400, 400);
+  background(220);
+  textSize(32);
+  text("this is the home screen.",20,40);
+ var activityOneButton =  createButton("activity 1");
+  activityOneButton.position(20,100);
+    activityOneButton.mousePressed(goTo("activityOne.html"));
+
+  activityTwoButton =  createButton("activity 2");
+  activityTwoButton.position(200,100);
 }
 
 function draw() {
-  background(220);
+   
+  
 }
+
+
+
