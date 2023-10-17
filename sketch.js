6e2f9e95120d2838c624521618aa2f1c67a1e881
TJStodda <99776279+TJStodda@users.noreply.github.com>
2023-10-16 18:33:36 -0700

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,9 @@
+//File sketches the canvas and has the draw function which is called 60 times per second by default
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
