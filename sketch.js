3c10f47cdb9566a07d6c038f8fe1f88724fee667
TJStodda <99776279+TJStodda@users.noreply.github.com>
2023-10-18 19:26:27 -0700

    Update sketch.js
    weird window.open error, still appears to work

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 //File sketches the canvas and has the draw function which is called 60 times per second by default
-//TODO: GET WORKING BUTTONS, WINDOW.OPEN()??
-var fileName;
+//Buttons are working,  window.open target is giving a weird error though
 
-function goTo(fileName) {
-  window.open(filename);
-}
+const target = "_self";
+
+var activityOneButton;
+var activityTwoButton;
+var activityThreeButton;
+var activityFourButton;
+var settingsButton;
+
 
 
 function setup() {
@@ -12,16 +16,28 @@ function setup() {
   background(220);
   textSize(32);
   text("this is the home screen.",20,40);
- var activityOneButton =  createButton("activity 1");
+   activityOneButton =  createButton("activity 1");
   activityOneButton.position(20,100);
-    activityOneButton.mousePressed(goTo("activityOne.html"));
+   
 
   activityTwoButton =  createButton("activity 2");
   activityTwoButton.position(200,100);
+  
+  activityThreeButton =  createButton("activity 3");
+  activityThreeButton.position(20,200);
+  
+  activityFourButton =  createButton("activity 4");
+  activityFourButton.position(200,200);
+   settingsButton =  createButton("settings");
+  settingsButton.position(300,300);
 }
 
 function draw() {
-   
+  activityOneButton.mousePressed(window.open("activityOne.html", target));
+//  activityTwoButton.mousePressed(window.open("activityTwo.html"));
+       // activityThreeButton.mousePressed(window.open("activityThree.html"));
+   // activityFourButton.mousePressed(window.open("activityFour.html"));
+// settingsButton.mousePressed(window.open("settings.html")); 
   
 }
 
