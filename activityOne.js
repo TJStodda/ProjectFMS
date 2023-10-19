39a234823ee993223114ce062bca1725cf41bada
TJStodda <99776279+TJStodda@users.noreply.github.com>
2023-10-18 19:27:33 -0700

    Update activityOne.js
    added back button(not finished yet)

--- activityOne.js
+++ activityOne.js
@@ -1,4 +1,6 @@
 //draws a basic circle for testing purposes
+
+var backButton;
 function setup(){
   createCanvas(400, 400);
  
@@ -6,6 +8,9 @@ function setup(){
 
 function draw(){
    background(220);
+  backButton = createButton();
+  backButton.position(300,300);
+  
   fill('#FFA524');
   circle(50,50,50);
 }
