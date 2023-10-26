e443ff6dc554705264c895646e9598390a281db6
TJStodda <99776279+TJStodda@users.noreply.github.com>
2023-10-25 19:32:57 -0700

    Update sketch.js
    Buttons work using new method, basic GUI achieved

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
-//File sketches the canvas and has the draw function which is called 60 times per second by default
-//Buttons are working,  window.open target is giving a weird error though
 
-const target = "_self";
+  
+//File sketches the canvas and has the draw function which is called 60 times per second by default
+
+
+
 
 var activityOneButton;
 var activityTwoButton;
@@ -12,32 +14,40 @@ var settingsButton;
 
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
   background(220);
+  
+  rect(180, 100, 240, 50,10,10,10,10)
   textSize(32);
-  text("this is the home screen.",20,40);
+  text("Main Menu",215,135);
    activityOneButton =  createButton("activity 1");
-  activityOneButton.position(20,100);
+  activityOneButton.position(15,300);
+  activityOneButton.size(120,50);
+ 
    
-
   activityTwoButton =  createButton("activity 2");
-  activityTwoButton.position(200,100);
+  activityTwoButton.position(165,300);
+  activityTwoButton.size(120,50);
   
   activityThreeButton =  createButton("activity 3");
-  activityThreeButton.position(20,200);
+  activityThreeButton.position(315,300);
+  activityThreeButton.size(120,50);
   
   activityFourButton =  createButton("activity 4");
-  activityFourButton.position(200,200);
+  activityFourButton.position(465,300);
+  activityFourButton.size(120,50);
    settingsButton =  createButton("settings");
-  settingsButton.position(300,300);
+  settingsButton.position(15,30);
+  settingsButton.size(120,50);
+  
 }
 
 function draw() {
-  activityOneButton.mousePressed(window.open("activityOne.html", target));
-//  activityTwoButton.mousePressed(window.open("activityTwo.html"));
-       // activityThreeButton.mousePressed(window.open("activityThree.html"));
-   // activityFourButton.mousePressed(window.open("activityFour.html"));
-// settingsButton.mousePressed(window.open("settings.html")); 
+  activityOneButton.mousePressed(window.open("https://editor.p5js.org/tjstodda/full/a5JBC5J6I"));
+ activityTwoButton.mousePressed(window.open("activityTwo.html"));
+        activityThreeButton.mousePressed(window.open("activityThree.html"));
+    activityFourButton.mousePressed(window.open("activityFour.html"));
+settingsButton.mousePressed(window.open("settings.html")); 
   
 }
 
