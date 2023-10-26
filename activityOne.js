a26249ad72df261a9ed6e888042744a95cd869cc
TJStodda <99776279+TJStodda@users.noreply.github.com>
2023-10-25 23:37:44 -0700

    Update activityOne.js

--- activityOne.js
+++ activityOne.js
@@ -1,4 +1,5 @@
 
+
 var backButton;
 function setup(){
   createCanvas(600, 400);
@@ -7,9 +8,11 @@ function setup(){
 
 function draw(){
    background(220);
+  textSize(32);
+  text("Activity One",250,150)
   backButton = createButton("back");
   backButton.position(300,300);
-  backButton.mousePressed(window.open("https://editor.p5js.org/tjstodda/full/92J081UKo",target ));
+  backButton.mousePressed(window.open("https://editor.p5js.org/tjstodda/full/92J081UKo" ));
   
   fill('#FFA524');
   circle(50,50,50);
